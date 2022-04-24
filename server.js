1a4aa72431d7d37f272f7d60a37320c5c2ea71fd
Jhonn Rodriguez <jhonn@wehaa.com>
2022-04-23 21:57:49 -0400

    Fix and add production env

--- server.js
+++ server.js
@@ -22,5 +22,12 @@ app.get('/', function (req, res) {
 const mongodb = require('./configs/database')
 mongodb.connect()
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/dist'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    })
+}
+
 app.listen(port, () => console.log("Server is running on port: " + port))
 
