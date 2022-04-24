6fd4c2b5c92cf09add78500f5ac448e377ee6551
Jhonn Rodriguez <jhonn@wehaa.com>
2022-04-23 22:23:30 -0400

    Remove route /home

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ app.use(bodyParser.json())
 const tasksRoutes = require('./routes/tasks')
 app.use('/tasks', tasksRoutes)
 
-app.get('/', function (req, res) {
-    res.json('Home')
-})
-
 const mongodb = require('./configs/database')
 mongodb.connect()
 
